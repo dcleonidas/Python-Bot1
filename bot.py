264609e6e70b43e68a321a659af724447c84f9f2
Tim <murrayta@mac.com>
2023-04-08 09:59:30 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,4 @@ load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#python', text="Pepega")
+client.chat_postMessage(channel='#python', text="Tim should go to the gym today....")
